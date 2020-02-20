a8f1fe7cc708bda1d3a9ebf50a0b621a61fbb96d
Chris Jones <chrisjones@localhost.localdomain>
2020-02-20 22:25:24 +0000

    refactor-200220: initial commit

--- themes/r3/src/index.js
+++ themes/r3/src/index.js
@@ -3,10 +3,9 @@ import { applyAnchors } from "./js/anchorify";
 import { activateListeners } from "./js/toc";
 import './sass/main.scss';
 
-applyAnchors();
-applySiteMapCollapse();
-
 document.addEventListener('DOMContentLoaded', function () {
+    applyAnchors();
+    applySiteMapCollapse();
     activateListeners();
 });
 
--- themes/r3/webpack.config.js
+++ themes/r3/webpack.config.js
@@ -38,6 +38,10 @@ module.exports = [
               },
             }
           ]
+        },
+        {
+            test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+            loader: 'url-loader'
         }
       ]
     },
