41b9b34bb3f23349044443904e7d6924755c05bf
jchrisjones <chris.jones@r3.com>
2020-02-26 15:28:37 +0000

    yarn-fix: toggle points the right way!
    * moved some style

--- themes/r3/scripts/sitemap.js
+++ themes/r3/scripts/sitemap.js
@@ -1,15 +1,15 @@
-
 /** Find page in site map and open collapsed nodes */
 function locatePageInSiteMap() {
-    var thisPage = document.URL.split('#')[0];
+    var thisPage = document.URL.split("#")[0];
     var sitemap = document.getElementById("r3-sitemap");
     var elements = sitemap.getElementsByTagName("a");
-    for (var i = 0 ; i < elements.length; i++) {
+    for (var i = 0; i < elements.length; i++) {
         if (thisPage == elements[i].href) {
             var e = elements[i];
-            while(e && e !== sitemap) {
+            while (e && e !== sitemap) {
                 if (e.classList.contains("nested")) {
                     e.classList.toggle("active");
+                    e.querySelector(".caret").classList.toggle("caret-down");
                 }
                 e = e.parentNode;
             }
@@ -21,15 +21,16 @@ function locatePageInSiteMap() {
 
 /** Applies leaf node toggle for collapse/show for the site tree */
 export function applySiteMapCollapse() {
+    locatePageInSiteMap();
     var toggler = document.getElementsByClassName("caret");
     var i;
 
     for (i = 0; i < toggler.length; i++) {
-        toggler[i].addEventListener("click", function () {
-            this.parentElement.querySelector(".nested").classList.toggle("active");
+        toggler[i].addEventListener("click", function() {
+            this.parentElement
+                .querySelector(".nested")
+                .classList.toggle("active");
             this.classList.toggle("caret-down");
         });
     }
-
-    locatePageInSiteMap();
 }
