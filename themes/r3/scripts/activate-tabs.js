b08f50d3456b29e4d127c2fef2d251636a96a45a
Barry Lapthorn <barry.lapthorn@r3.com>
2020-02-26 09:13:03 +0000

    Fix query tab errors, and run hugo and yarn from one command.
    Use node to run a runner script that in turn start yarn watch and hugo

---
+++ themes/r3/scripts/activate-tabs.js
@@ -0,0 +1,16 @@
+//  We want scripts to load last, including jquery.
+//  Once jquery is loaded we can then 'activate' all the tabs.
+
+import $ from 'jquery';
+import 'jquery-ui';
+import 'jquery-ui/ui/widgets/tabs';
+
+export function activateTabs() {
+    var elements = document.getElementsByClassName('r3-o-tabs');
+    for (var i = 0; i < elements.length; i++) {
+        var e = elements[i];
+        var s = "#" + e.id;
+        $(function () { $(s).tabs(); });
+        console.log(s);
+    }
+}
