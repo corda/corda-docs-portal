cccf7f9d6be0b79ea2ffba324b9fcd5b1a8ea8bb
jchrisjones <chris.jones@r3.com>
2020-04-23 15:12:54 +0100

    Minimist-fix: delete yarn.lock & package-lock.json (#18)
    * commit to rebase with correct branch
    
    * Minimist-fix: delete yarn.lock & package-lock.json
    
    * ran npm update
    
    * removed package-lock again
    
    * ran npm install
    
    * npm audit
    
    * acorn versions 6.4.1 & 7.11
    
    * minimist version 1.2.5
    
    * Minimist-fix: cleaner webpack build
    
    * Minimist-fix: app.js and vender.js rebuilt
    
    * done in previous commit
    
    * edit of the gitignore file
    
    * rebuilt the static files
    
    * Minimist Fix: removed mermaid.js from build
    
    Co-authored-by: Chris Jones <jchrisjones@gmail.com>
    Co-authored-by: ivanterziev <61829352+ivanterziev-r3@users.noreply.github.com>

--- themes/hugo-r3-theme/scripts/vendor/index.js
+++ themes/hugo-r3-theme/scripts/vendor/index.js
@@ -1,5 +1,4 @@
 import "./bootstrap-init";
 import docsearch from "docsearch.js";
 
-// 'export' docsearch to the web page so we can call docsearch
 window.docsearch = docsearch;
--- themes/hugo-r3-theme/webpack.config.js
+++ themes/hugo-r3-theme/webpack.config.js
@@ -1,54 +1,48 @@
 const path = require("path");
 
-module.exports = [
-    {
-        entry: {
-            app: ["./scripts", "./styles/scss/main.scss"],
-            vendor: "./scripts/vendor"
-        },
-        output: {
-            path: path.resolve(__dirname, "./static/"),
-            filename: "js/[name].js"
-        },
-        devtool: "source-map",
-        module: {
-            rules: [
-                {
-                    test: /\.(js|jsx)$/,
-                    exclude: /node_modules/,
-                    use: ["babel-loader", "eslint-loader"]
-                },
-                {
-                    test: /\.scss$/,
-                    use: [
-                        {
-                            loader: "file-loader",
-                            options: {
-                                name: "css/[name].css"
-                            }
-                        },
-                        "extract-loader",
-                        {
-                            loader: "css-loader",
-                            options: {
-                                url: false
-                            }
-                        },
-                        "sass-loader"
-                    ]
-                },
-                {
-                    test: /\.(png|jpg|gif)$/,
-                    use: [
-                        {
-                            loader: "url-loader",
-                            options: {
-                                limit: 5000
-                            }
+module.exports = [{
+    entry: {
+        app: ["./scripts", "./styles/scss/main.scss"],
+        vendor: "./scripts/vendor"
+    },
+    output: {
+        path: path.resolve(__dirname, "./static/"),
+        filename: "js/[name].js"
+    },
+    devtool: "source-map",
+    module: {
+        rules: [{
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader", "eslint-loader"]
+            },
+            {
+                test: /\.scss$/,
+                use: [{
+                        loader: "file-loader",
+                        options: {
+                            name: "css/[name].css"
                         }
-                    ]
-                }
-            ]
-        }
+                    },
+                    "extract-loader",
+                    {
+                        loader: "css-loader",
+                        options: {
+                            url: false
+                        }
+                    },
+                    "sass-loader"
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [{
+                    loader: "url-loader",
+                    options: {
+                        limit: 5000
+                    }
+                }]
+            }
+        ]
     }
-];
+}];
