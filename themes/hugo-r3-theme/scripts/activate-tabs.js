a8f0f99f599c3e0001ffb1d0a1343391a8af6e53
ivanterziev <61829352+ivanterziev-r3@users.noreply.github.com>
2020-04-29 15:48:01 +0100

    Revert "EG-1602 Fix code tabs (#39)" (#42)
    This reverts commit 2b4ec80501860bbc87a5c4ec50a616247a7f92bd.

--- themes/hugo-r3-theme/scripts/activate-tabs.js
+++ themes/hugo-r3-theme/scripts/activate-tabs.js
@@ -4,24 +4,12 @@
 import $ from 'jquery';
 import 'jquery-ui';
 import 'jquery-ui/ui/widgets/tabs';
-import 'bootstrap';
-
-const default_tab = "java";
 
 export function activateTabs() {
     var elements = document.getElementsByClassName('r3-o-tabs');
     for (var i = 0; i < elements.length; i++) {
         var e = elements[i];
         var s = "#" + e.id;
-        var default_tab_name = s + '-' + default_tab;
-
-        // https://getbootstrap.com/docs/4.4/components/navs/#tabshow
-
-        // Always make the first tab the visible one
-        $(s + ' a').first().tab('show');
-
-        // Unless it's the default tab i.e. 'java', in which case, show that instead
-        var selector = s + ' a[href="' + default_tab_name + '"]';
-        $(selector).tab('show');
+        $(function () { $(s).tabs(); });
     }
 }
