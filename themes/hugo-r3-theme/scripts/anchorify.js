d55e95c5a58124b1d2705ffb69a132eecf7dcbb3
jchrisjones <chris.jones@r3.com>
2021-05-06 17:53:52 +0100

    jchrisjones/fix/update-packages: All packages are at the latest (#451)
    * Webpack.config.js was given a complete rewrite.
    
    * images in scss files were converted to base64
    
    Co-authored-by: ivanterziev <61829352+ivanterziev-r3@users.noreply.github.com>

--- themes/hugo-r3-theme/scripts/anchorify.js
+++ themes/hugo-r3-theme/scripts/anchorify.js
@@ -1,4 +1,4 @@
-var anchorForId = function(id) {
+var anchorForId = function (id) {
     var anchor = document.createElement("a");
     anchor.className = "header-link";
     anchor.href = "#" + id;
@@ -6,7 +6,7 @@ var anchorForId = function(id) {
     return anchor;
 };
 
-var linkifyAnchors = function(level, containingElement) {
+var linkifyAnchors = function (level, containingElement) {
     var headers = containingElement.getElementsByTagName("h" + level);
     for (var h = 0; h < headers.length; h++) {
         var header = headers[h];
@@ -19,7 +19,7 @@ var linkifyAnchors = function(level, containingElement) {
 
 /**  This applies anchors to all content headings.  */
 export function applyAnchors() {
-    document.onreadystatechange = function() {
+    document.onreadystatechange = function () {
         if (this.readyState === "complete") {
             var contentBlock = document.getElementsByClassName("r3-content")[0];
             if (!contentBlock) {
--- themes/hugo-r3-theme/scripts/vendor/doc-search.js
+++ themes/hugo-r3-theme/scripts/vendor/doc-search.js
@@ -3,16 +3,16 @@ const algolia_apiKey = "1fe3367db02689b4aeebc59efad5abaf";
 const algolia_index = "docs.corda.net";
 const facetFilters = window.facetFilters;
 
-export function docSearchInit(){
-    if (document.querySelector('#search-input')) {
+export function docSearchInit() {
+    if (document.querySelector("#search-input")) {
         //https://www.algolia.com/doc/api-reference/api-parameters/facetFilters/?language=javascript
         let algoliaOptions = {
             hitsPerPage: 30,
             facetFilters: facetFilters
         };
 
-        if(/404.html/.test(window.location.pathname)){
-           delete algoliaOptions.facetFilters;
+        if (/404.html/.test(window.location.pathname)) {
+            delete algoliaOptions.facetFilters;
         }
 
         window.docsearch({
