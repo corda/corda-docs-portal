a59714dce27f514af21946eff5ba427bdc8c69f9
Peter Bienek <peter.bienek@r3.com>
2023-02-24 16:34:34 +0000

    Second attempt to fix missing class issue in prod. Perhaps the compiler doesn't like double hyphens in class names.

--- themes/doks/assets/js/banner-message.js
+++ themes/doks/assets/js/banner-message.js
@@ -1,14 +1,14 @@
 const timeoutDuration = 8000 //milliseconds
 const $bannerMessagesContainer = document.querySelector('[data-banner-messages-container]')
 const numberOfMessages = $bannerMessagesContainer.children.length
-$bannerMessagesContainer.children[0].classList.add("banner-message__item--active")
+$bannerMessagesContainer.children[0].classList.add("banner-message__item-active")
 
 if (numberOfMessages > 0) {
   let currentMessage = 0;
   setInterval(() => {
     let nextMessage = (currentMessage === numberOfMessages - 1) ? 0 : currentMessage + 1
-    $bannerMessagesContainer.children[currentMessage].classList.remove("banner-message__item--active")
-    $bannerMessagesContainer.children[nextMessage].classList.add("banner-message__item--active")
+    $bannerMessagesContainer.children[currentMessage].classList.remove("banner-message__item-active")
+    $bannerMessagesContainer.children[nextMessage].classList.add("banner-message__item-active")
     currentMessage = nextMessage
   }, timeoutDuration)
 }
